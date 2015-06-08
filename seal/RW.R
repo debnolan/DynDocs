377b78431e4c0892d5ad22eb46f200987a3e56c3
Donovan Lieu <dlieu333@berkeley.edu>
2015-06-08 14:57:26 -0700

    RW.R added

---
+++ seal/RW.R
@@ -0,0 +1,24 @@
+OneDimSimpleRandWalk <- function(num.steps, prob = 0.5) {
+  return(cumsum(sample(c(1, -1), num.steps, TRUE)))
+}
+
+OneDimBrownianRandWalk <- function(num.steps, mean = 0, sd = 1) {
+  return(cumsum(rnorm(num.steps, mean, sd)))
+}
+
+TwoDimSimpleRandWalk <- function(num.steps, x.prob = 0.5, y.prob = 0.5) {
+  pos = matrix(0, ncol = 2, nrow = num.steps)
+  dir = sample(c(1, 2), num.steps, TRUE)
+  move = sample(c(1, -1), num.steps, TRUE)
+  pos[cbind(1:num.steps, dir)] = move
+  pos = apply(pos, 2, cumsum)
+  return(pos)
+}
+
+TwoDimBrownianRandWalk <- function(num.steps, mean = 0, sd = 1) {
+  return(cbind(OneDimBrownianRandWalk(num.steps, mean, sd),
+               OneDimBrownianRandWalk(num.steps, mean, sd)))
+}
+
+TDSRW = TwoDimSimpleRandWalk(100000)
+plot(TDSRW, type = "l")
