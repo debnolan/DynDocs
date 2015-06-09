91ae5fa9b45d0a2d1e6f73c726eb0a4ba4901b5e
Zoe <Zeyu@Zeyus-MacBook-Air.local>
2015-06-08 23:16:17 -0700

    making side by side scatterplot using Shiny and ggplot

--- SAT/shiny_plot1_0.1/my_app/server.R
+++ SAT/shiny_plot1_0.1/my_app/server.R
@@ -3,26 +3,28 @@ library(shiny)
 load("satDF_new.rda")
 # Note! Need to rename the variables
 library(ggplot2)
+library(grid)
 
 # Define server logic required to draw a scatterplot
 shinyServer(function(input, output) {
-  formulaText <- reactive(function() {
-    paste(input$y, "vs.", input$x)
-  })
-  
-  # Return the formula text for printing as a caption
-  output$caption <- reactiveText(function() {
-    formulaText()
-  })
+#   formulaText <- reactive(function() {
+#     paste(input$y, "vs.", input$x)
+#   })
+#   
+#   # Return the formula text for printing as a caption
+#   output$caption <- reactiveText(function() {
+#     formulaText()
+#   })
   
   output$text <- renderText({
     input$title
-  })  
+  })
+  
   output$scatterPlot <- renderPlot({
    x <- satDF_new[ , input$x]
    y <- satDF_new[ , input$y]
    
-   # par(mar = c(5.1, 4.1, 0, 1))
+   par(mar = c(5.1, 4.1, 0, 1))
    # plot(x, y, pch = 19, col = "blue", cex = 2) 
   
    dat <- data.frame(x, y, Region = satDF_new$region, Population = satDF_new$StatePopulation, 
@@ -30,8 +32,17 @@ shinyServer(function(input, output) {
 
    p <- ggplot(dat, aes(x, y, size = Population), guide = FALSE)
    p + geom_point(aes(colour = Region)) + 
-     xlab(input$x) +
-     ylab(input$y)
+      xlab(input$x) +
+      ylab(input$y) + 
+      ggtitle(paste(input$y, "vs.", input$x)) +
+      theme(axis.title.x=element_text(vjust = -1)) +
+      theme(axis.title.y=element_text(angle = 90, vjust = 1)) +
+      theme(plot.title = element_text(colour = "black")) +
+      theme(plot.title=element_text(size = 15, vjust = 3)) 
+     
+#      + theme(plot.margin = unit(c(0,0,0,0), "cm"))
+#      + theme(legend.background = element_rect(), legend.margin = unit(0, "cm"))
+#      
    
 
    
