d51e35630119e03ef7b15f0fb3b0bd0ef44f03c9
josephschenker <josephschenker@berkeley.edu>
2015-06-10 22:42:08 -0700

    Draft of Analysis Document

--- DrugTrials/trialAnalysis/helpers.R
+++ DrugTrials/trialAnalysis/helpers.R
@@ -19,5 +19,42 @@ pValPlot = function(pop1, pop2, comp = 5, min, max, freq = .1) {
   title(main = paste("pValue =", as.character(pHat)))
 }
 
+dichPlot = function(pop1, pop2, NRep, thresh) {
+  pop = c(pop1, pop2)
+  dich = as.numeric(pop >= thresh)
+  simPop = replicate(NRep, sum(sample(dich, length(pop1))))
+  plot(table(unique(simPop, simPop)), type = "h",
+       xlab = "Number of Samples Greater than Threshold",
+       ylab = "Number of Times")
+  compare = simPop >= length(pop1)
+  pVal = sum(compare)/NRep
+  title(main = paste("pValue =", as.character(pVal)))
+}
+
+dichData = function(pop1, pop2, threshold) {
+  return(data.frame(calcium = c(pop1, NA), 
+                    dich1 = c(as.numeric(pop1 >= threshold), NA),
+                    placebo = pop2, dich2 = as.numeric(pop2 >= threshold)))
+}
+
+wilPlot = function(pop1, pop2, NRep, comp) {
+  pop = rank(c(pop1, pop2))
+  simPop = replicate(NRep, mean(sample(pop, length(pop1))))
+  plot(table(floor(unique(simPop, simPop))), type = "h",
+       xlab = "Ranked Mean of Blood Pressure Change",
+       ylab = "Number of Times")
+  compare = simPop >= pop[which(c(pop1, pop2) == comp)][1] 
+  pVal = sum(compare)/NRep
+  title(main = paste("pValue =", as.character(pVal)))
+}
+
+rankData = function(pop1, pop2, threshold) {
+  return(data.frame(calcium = c(pop1, NA), 
+            rankedCalcium = c(rank(c(pop1, pop2))[1:length(pop1)], NA),
+            placebo = pop2, 
+            rankedPlacebo = rank(c(pop1, pop2))[length(pop1)+1:length(pop2)])
+              )
+}
+
 calcium = c(7, -4, 18, 17, -3, -5, 1, 10, 11, -2)
 placebo = c(-1, 12, -1, -3, 3, -5, 5, 2, -11, -1, -3)
--- DrugTrials/trialAnalysis/server.R
+++ DrugTrials/trialAnalysis/server.R
@@ -3,6 +3,12 @@ source("helpers.R")
 
 shinyServer(
   function(input, output) {
+    output$data <- renderTable({
+      data.frame(calcium = c(calcium, NA), placebo = placebo)
+    })
+    output$compare <- renderPrint({
+      c(mean(calcium), mean(placebo))
+    })
     output$perm <- renderPlot({
     args <- list(NRep = input$n, comp = input$k,
                    pop1 = calcium, pop2 = placebo)
@@ -15,6 +21,26 @@ shinyServer(
                    pop1 = calcium, pop2 = placebo)
       
       do.call(pValPlot, args)
+    })
+    
+    output$dataDich <- renderTable({
+      dichData(calcium, placebo, thresh = input$v)
+    })
+    
+    output$dich <- renderPlot({
+      args <- list(NRep = input$r, thresh = input$v,
+                   pop1 = calcium, pop2 = placebo)
+      do.call(dichPlot, args)
+    })
+    
+    output$rank <- renderTable({
+      rankData(calcium, placebo, threshold = input$t)
+    })
+    
+    output$wilcox <- renderPlot({
+      args <- list(NRep = input$p, comp = input$t,
+                   pop1 = calcium, pop2 = placebo)
+      do.call(wilPlot, args)
     })
   }
 )
