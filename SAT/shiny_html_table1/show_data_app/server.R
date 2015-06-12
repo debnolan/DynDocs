20e0d55eb1651b6c04724f79c58f8837552a796f
Zoe <Zeyu@airbears2-10-142-40-249.AirBears2.1918.Berkeley.EDU>
2015-06-12 16:31:01 -0700

    fixed the problem in html version of show-data app by removing the fancy html-widget for table display, further data entry cleaning may be needed.

--- SAT/shiny_html_table1/show_data_app/server.R
+++ SAT/shiny_html_table1/show_data_app/server.R
@@ -1,12 +1,12 @@
 # setwd("~/Dropbox/Senior_Research/Stat_Summer15_Research/DynDocs/SAT/shiny_table1_0.1/")
-library(shiny)
 library(ggplot2)
+library(DT)
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
   load("data/satDF_pretty.rda")
   
-  output$mytable1 = renderDataTable({
+  output$mytable1 = renderTable({
     satDF_pretty
   }, options = list(orderClasses = TRUE))
   
