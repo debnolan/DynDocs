046989954c87bdcbe13caf0dac5aedc5a0ecb0a4
Fionn616 <wanting_616@msn.com>
2015-06-22 20:17:02 -0700

    analysis.r

--- vehicleSurvey/analysis.r
+++ vehicleSurvey/analysis.r
@@ -1,6 +1,6 @@
 ## read data
-vehical = read.csv("vius.csv")
-ca = read.csv("viusca.csv", header = FALSE)
+vehical = read.csv(file="~/Desktop/Dyn/DynDocs/vehicleSurvey/vius.csv")
+ca = read.csv("~/Desktop/Dyn/DynDocs/vehicleSurvey/viusca.csv", header = FALSE)
 names(ca) = names(vehical)
 hist(ca$MILES_ANNL, breaks = 30)
 
@@ -49,4 +49,64 @@ plot(density(bootMed))
 ## 95% confidence interval for the median of truck miles driven in 2002 in CA
 quantile(bootMed, c(0.025, 0.975))
 
+var(bootMed)
 
+
+
+
+
+
+
+##write a function for bootsrap
+straca = as.data.frame(cbind(ca$TRUCKTYPE, ca$TABTRUCKS, ca$MILES_ANNL))
+colnames(straca) = c("type", "weight", "annlmile")
+
+med.vehical = function(data){
+  samp = vector()
+  l = as.vector(table(data$type))
+  weight = unique(data$weight)
+  for(i in 1:5){
+    samp = c(samp, sample(x = data[data$type == i, "annlmile"], 
+                                size = l[i] - 1, replace = T))
+  } 
+  sampdf = data.frame(samp = samp, type = rep(1:5, l - 1),
+                      weights = rep(weight * l/(l - 1), l - 1))
+  ##load the library first
+  truckdesign = svydesign(id = ~1, strata = ~type, weights = ~weights, data = sampdf)
+  sampMed = svyquantile(~samp, truckdesign, 0.50)
+  return(sampMed)
+}
+
+##step test for the function
+res = vector()
+length = as.vector(table(straca$type))
+weights = unique(straca$weight)
+for(i in 1:5){
+  res = c(res, sample(x = straca[straca$type == i, "annlmile"], 
+                      size = length[i] - 1 , replace = T))
+} 
+sampdf = data.frame(samp = res, type = rep(1:5, length - 1),
+                    weights = rep(weights * length/(length - 1), length - 1))
+
+truckdesign = svydesign( id = ~1, strata = ~type, weights = ~weights, data = sampdf)
+sampMed = svyquantile(~samp, truckdesign, 0.50)
+
+###run the function
+med.vehical(straca)
+
+
+##rep the function for 500 times
+boot = replicate(500, med.vehical(straca))
+
+plot(density(boot))
+## 95% confidence interval for the median of truck miles driven in 2002 in CA
+quantile(boot, c(0.025, 0.975))
+var(boot)
+
+
+
+cd
+git pull
+git add ....r
+git commit -m "...r"
+git push
