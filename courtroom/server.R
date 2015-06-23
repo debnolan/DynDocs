b0015fed996e9e49af04af42f30012edb4e21b66
victorkong94 <victorkong@berkeley.edu>
2015-06-23 00:12:39 -0700

    Add R files for server logic

---
+++ courtroom/server.R
@@ -0,0 +1,27 @@
+library(shiny)
+
+source("helpers.R")
+
+logic = function(input, output) {
+  
+  # Computes table of observed values from from client input
+  counts = reactive({
+  
+    # Determines which of the four functions to use from helpers.R
+    tabGen = switch(input$whichCell,
+                    cell11 = tabGen11,
+                    cell21 = tabGen21,
+                    cell12 = tabGen12,
+                    cell22 = tabGen22,
+                    tabGen11)
+    tabGen(input$cellCount)
+  })
+  
+  # Generates plots and summary of test for display output
+  output$chiSqPlot = renderPlot(plotGen(counts))
+  output$residPlot = renderPlot(residPlot(counts))
+  output$summary = renderText(conclusion(counts))
+  
+}
+
+shinyServer(logic)
