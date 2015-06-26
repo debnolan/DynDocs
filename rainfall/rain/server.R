3673eb1facbf48aa0a549a915c46b9312587ba6a
Wuji Zhang <zwj.stat@gmail.com>
2015-06-26 03:16:18 -0700

    map downloaded, scolling problem fixed, and new bg color

---
+++ rainfall/rain/server.R
@@ -0,0 +1,100 @@
+library(shiny)
+library(ggmap)
+library(extRemes)
+source("helpers.R")
+options(digits = 9)
+shinyServer(function(input, output,session) {
+  # Return the requested dataset
+  datasetInput <- reactive({
+    switch(input$station,
+           "Weather station 1" =1, "Weather station 2" =2, "Weather station 3" =3, "Weather station 4" =4,
+           "Weather station 5" =5, "Weather station 6" =6, "Weather station 7" =7, "Weather station 8" =8, 
+           "Weather station 9" =9, "Weather station 10" =10, "Weather station 11" =11, "Weather station 12" =12,
+           "Weather station 13" =13, "Weather station 14" =14, "Weather station 15" =15, "Weather station 16" =16,
+           "Weather station 17" =17, "Weather station 18" =18, "Weather station 19" =19, "Weather station 20" =20,
+           "Weather station 21" =21, "Weather station 22" =22, "Weather station 23" =23, "Weather station 24" =24,
+           "Weather station 25" =25, "Weather station 26" =26, "Weather station 27" =27, "Weather station 28" =28,
+           "Weather station 29" =29, "Weather station 30" =30, "Weather station 31" =31, "Weather station 32" =29,
+           "Weather station 33" =33, "Weather station 34" =34, "Weather station 35" =35, "Weather station 36" =36,
+           "Weather station 37" =37, "Weather station 38" =38, "Weather station 39" =39, "Weather station 40" =40,
+           "Weather station 41" =41, "Weather station 42" =42, "Weather station 43" =43, "Weather station 44"= 44,
+           "Weather station 45" =45, "Weather station 46" =46, "Weather station 47" =47, "Weather station 48"= 48,
+           "Weather station 49" =49, "Weather station 50" =50, "Weather station 51" =51, "Weather station 52" =52,
+           "Weather station 53"= 53, "Weather station 54" =54, "Weather station 55" =55, "Weather station 56" =56)
+  })
+  datasetInput2 <- reactive({
+    switch(input$station2,
+           "Weather station 1" =1, "Weather station 2" =2, "Weather station 3" =3, "Weather station 4" =4,
+           "Weather station 5" =5, "Weather station 6" =6, "Weather station 7" =7, "Weather station 8" =8, 
+           "Weather station 9" =9, "Weather station 10" =10, "Weather station 11" =11, "Weather station 12" =12,
+           "Weather station 13" =13, "Weather station 14" =14, "Weather station 15" =15, "Weather station 16" =16,
+           "Weather station 17" =17, "Weather station 18" =18, "Weather station 19" =19, "Weather station 20" =20,
+           "Weather station 21" =21, "Weather station 22" =22, "Weather station 23" =23, "Weather station 24" =24,
+           "Weather station 25" =25, "Weather station 26" =26, "Weather station 27" =27, "Weather station 28" =28,
+           "Weather station 29" =29, "Weather station 30" =30, "Weather station 31" =31, "Weather station 32" =29,
+           "Weather station 33" =33, "Weather station 34" =34, "Weather station 35" =35, "Weather station 36" =36,
+           "Weather station 37" =37, "Weather station 38" =38, "Weather station 39" =39, "Weather station 40" =40,
+           "Weather station 41" =41, "Weather station 42" =42, "Weather station 43" =43, "Weather station 44"= 44,
+           "Weather station 45" =45, "Weather station 46" =46, "Weather station 47" =47, "Weather station 48"= 48,
+           "Weather station 49" =49, "Weather station 50" =50, "Weather station 51" =51, "Weather station 52" =52,
+           "Weather station 53"= 53, "Weather station 54" =54, "Weather station 55" =55, "Weather station 56" =56)
+  })
+  
+  
+  output$mapImage <- renderImage({
+    # When datasetInput() is 3, filename is www/images/3.png
+    filename <- normalizePath(file.path('www/images',
+                                        paste(datasetInput(), '.png', sep='')))
+    list(src = filename)
+  }, deleteFile = FALSE)
+  
+  output$mapImage2 <- renderImage({
+    filename <- normalizePath(file.path('www/images',
+                                        paste(datasetInput2(), '.png', sep='')))
+    list(src = filename)
+  }, deleteFile = FALSE)
+  
+  
+  output$plots3 <- renderPlot({
+    thresh = quantile(rainfall[[datasetInput()]]$precip, input$threshold/100)
+    which(rainfall[[datasetInput()]]$precip>thresh)
+    par(bg = '#FAFAD2')
+    plot(rainfall[[datasetInput()]]$precip[which(rainfall[[datasetInput()]]$precip>thresh)]~rainfall[[datasetInput()]]$time[which(rainfall[[datasetInput()]]$precip>thresh)],
+         ylim = range(rainfall[[datasetInput()]]$precip),
+         xlab = "", ylab = "Precipitation (mm)", main = paste("Daily precipitation of weather station", datasetInput()), col = 'darkblue')
+    points(rainfall[[datasetInput()]]$precip[which(rainfall[[datasetInput()]]$precip<=thresh)]~rainfall[[datasetInput()]]$time[which(rainfall[[datasetInput()]]$precip<=thresh)],
+           col = 'black')
+    abline(h = quantile(rainfall[[datasetInput()]]$precip, input$threshold/100), col = 'red', lwd = 2)
+  })
+  
+  
+  
+  output$plots2 <- renderPlot({
+    fit = fevd(precip, rainfall[[datasetInput()]], threshold = quantile(rainfall[[datasetInput()]]$precip, input$threshold/100), type="GP", units = "mm")
+    par(mfrow = c(2,2), bg = '#FAFAD2')
+    plot(fit, "probprob", main ="Probability Plot")
+    plot(fit, "qq", main ="Quantile Plot")
+    plot(fit, type = "rl", rperiods= c(2,5,10,20,50,80,100,120,200), main ="Return Level Plot")
+    plot(fit, type = "density", main = "Density Plot")
+  })
+  
+  sim <- reactive({
+    input$goButton
+    isolate({fevd(precip, rainfall[[datasetInput2()]],
+                  threshold = quantile(rainfall[[datasetInput2()]]$precip, input$threshold2/100),
+                  type="GP", units = "mm")
+    })
+  })
+  output$rl2 <- renderText({
+    RL2 = ci(sim(), return.period=c(2,5,10,20,50,80,100,120,200))
+    paste(capture.output(myprint(RL2))[-(1:3)], collapse = "\n")
+  })
+  
+  output$plots4 <- renderPlot({
+    par(bg = '#FAFAD2')
+    plot(sim(),"qq2")
+    title(paste("Simulation result (Weather station ", datasetInput2(), ")", sep = ""))
+  })
+  
+  
+})
---
+++ rainfall/rain/ui.R
@@ -0,0 +1,96 @@
+shinyUI(navbarPage("Rainfall in Colorado",
+
+# Analysis Tab                   
+                   tabPanel("Analysis", 
+                            div(class="outer",
+                                 tags$head(
+                                   # Include our custom CSS on this Panel
+                                   includeCSS("www/styles.css")
+                                 ),
+                                fluidPage(
+                                  fluidRow(
+                                    column(8,
+                                           plotOutput("plots3"),
+                                           hr(),
+                                           plotOutput("plots2")
+                                    )
+                                  )
+                                ),
+                                tags$head(tags$style(
+                                  type="text/css",
+                                  "#mapImage img {max-width: 100%; width: 100%; height: 93%}"
+                                )),
+                                absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                              draggable = FALSE, top = 80, left = "auto", right = 30, bottom = "auto",
+                                              width = "30%", height = "auto",
+                                              h2("Data input"),
+                                              selectInput("station", 
+                                                          label = "Choose a weather station:",
+                                                          choices = c("Weather station 1", "Weather station 2", "Weather station 3", "Weather station 4", "Weather station 5", "Weather station 6", "Weather station 7", "Weather station 8", "Weather station 9", "Weather station 10",
+                                                                      "Weather station 11", "Weather station 12", "Weather station 13", "Weather station 14", "Weather station 15", "Weather station 16", "Weather station 17", "Weather station 18", "Weather station 19", "Weather station 20",
+                                                                      "Weather station 21", "Weather station 22", "Weather station 23", "Weather station 24", "Weather station 25", "Weather station 26", "Weather station 27", "Weather station 28", "Weather station 29", "Weather station 30",
+                                                                      "Weather station 31", "Weather station 32", "Weather station 33", "Weather station 34", "Weather station 35", "Weather station 36", "Weather station 37", "Weather station 38", "Weather station 39", "Weather station 40",
+                                                                      "Weather station 41", "Weather station 42", "Weather station 43", "Weather station 44", "Weather station 45", "Weather station 46", "Weather station 47", "Weather station 48", "Weather station 49", "Weather station 50",
+                                                                      "Weather station 51", "Weather station 52", "Weather station 53", "Weather station 54", "Weather station 55", "Weather station 56"),
+                                                          selected = "Weather station 1"),
+                                              sliderInput("threshold", "Threshold:", 
+                                                          min = 80, max = 100, value = 95, step = 0.5),
+                                              helpText("Note: this is the quantile at which the threshold is set. "),
+#                                               tags$div(class = "ZoomIt", 
+#                                                        tags$img, )
+imageOutput("mapImage")
+                                ),
+                                tags$div(id="cite",
+                                         'Summer project,', tags$em('by Wuji and Ryan.'))
+                            )
+                   ),
+
+
+
+#  Simulation Tab
+
+
+                   tabPanel("Simulation",
+                            div(class="outer",
+                                tags$head(
+                                  # Include our custom CSS on this Panel
+                                  includeCSS("www/styles.css")
+                                ),
+                                fluidPage(
+                                  fluidRow(
+                                    column(8,
+                                           plotOutput("plots4"),
+                                           hr(),
+                                           verbatimTextOutput("rl2")
+                                    )
+                                  )
+                                ),
+                                tags$head(tags$style(
+                                  type="text/css",
+                                  "#mapImage2 img {max-width: 100%; width: 100%; height: 93%}"
+                                )),
+                                absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                              draggable = FALSE, top = 80, left = "auto", right = 30, bottom = "auto",
+                                              width = "30%", height = "auto",
+                                              h2("Data input"),
+                                              selectInput("station2", 
+                                                          label = "Choose a weather station:",
+                                                          choices = c("Weather station 1", "Weather station 2", "Weather station 3", "Weather station 4", "Weather station 5", "Weather station 6", "Weather station 7", "Weather station 8", "Weather station 9", "Weather station 10",
+                                                                      "Weather station 11", "Weather station 12", "Weather station 13", "Weather station 14", "Weather station 15", "Weather station 16", "Weather station 17", "Weather station 18", "Weather station 19", "Weather station 20",
+                                                                      "Weather station 21", "Weather station 22", "Weather station 23", "Weather station 24", "Weather station 25", "Weather station 26", "Weather station 27", "Weather station 28", "Weather station 29", "Weather station 30",
+                                                                      "Weather station 31", "Weather station 32", "Weather station 33", "Weather station 34", "Weather station 35", "Weather station 36", "Weather station 37", "Weather station 38", "Weather station 39", "Weather station 40",
+                                                                      "Weather station 41", "Weather station 42", "Weather station 43", "Weather station 44", "Weather station 45", "Weather station 46", "Weather station 47", "Weather station 48", "Weather station 49", "Weather station 50",
+                                                                      "Weather station 51", "Weather station 52", "Weather station 53", "Weather station 54", "Weather station 55", "Weather station 56"),
+                                                          selected = "Weather station 1"),
+                                              sliderInput("threshold2", "Threshold:", 
+                                                          min = 80, max = 100, value = 95),
+                                              helpText("Note: this is the quantile at which the threshold is set. "),
+                                              imageOutput("mapImage2"),
+                                              actionButton("goButton", "Re-run simulation", icon("random")),
+                                              helpText("Generate a QQ plot of quantiles from model-simulated data against the data.")
+                                ),
+                                tags$div(id="cite",
+                                         'Summer project,', tags$em('by Wuji and Ryan.'))
+                            )
+                   )
+))
