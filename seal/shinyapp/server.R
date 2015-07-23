0b99a85aa2748bbdf51e6b094fe956a41acefb48
chanik90 <chanikjang90@gmail.com>
2015-07-22 17:11:19 -0700

    updata

--- seal/shinyapp/server.R
+++ seal/shinyapp/server.R
@@ -4,159 +4,150 @@ source("RW.R")
 
 shinyServer(function(input, output) {
   
-  ######1D SIMPLE RANDOM WALK  
-    if (FALSE) {
-    # Return the requested dataset
-    typesInput <- reactive({
-      input$typesB
+    
+######1D SIMPLE RANDOM WALK  
+
+  plotParamsB <- reactiveValues(stepsB = 1, probB=0.5, numbB=1, typesInputB = NA) 
+    observeEvent(input$runSimB, {
+                plotParamsB$stepsB <- input$stepsB
+                plotParamsB$probB <- input$probB 
+                plotParamsB$numbB <- input$numbB
+                plotParamsB$typesInputB <- OneDimBrownianRandWalk(plotParamsB$stepsB,
+                                                                  plotParamsB$probB) 
     })
-    # num.steps <- reactive({
-    #  as.numeric(input$steps)
-    #  })
-    # Generate a random walk plot
-  }
   
-  plotParamsB <- reactiveValues(stepsB = 1, typesInputB = NA)
-  
-  observeEvent(input$runSimB, {
-    plotParamsB$stepsB <- input$stepsB
-    
-    plotParamsB$typesInputB <- OneDimSimpleRandWalk(plotParamsB$stepsB)     
-    
-    
-  })
-  
-  output$plotB <- renderPlot({  
-    
-    if (FALSE){
-      num.steps <- input$stepsB
-      typesInput <- OneDimSimpleRandWalk(num.steps)     
-      
-      
-    }
+  output$plotB <- renderPlot({    
     
     #### plot(xx$typesInputA ,type="l") 
     if (is.na(plotParamsB$typesInputB )) return(NULL)
-    plot(plotParamsB$typesInputB ,type="l")     
-  })
+    multiWalk(function(x) OneDimSimpleRandWalk(x, plotParamsB$probB), plotParamsB$stepsB, plotParamsB$numbB)     
+    #plot(plotParamsB$typesInputB, type="l")     
+    })
 
-  
-  
 ######1D BROWNIAN RANDOM WALK  
   
-  plotParamsC <- reactiveValues(stepsC = 1, meanC = 0, sdC = 1, typesInputC = NA) 
-  observeEvent(input$runSimC, {
-    plotParamsC$meanC <- input$meanC
-    plotParamsC$sdC <- input$sdC
-    plotParamsC$stepsC <- input$stepsC
-    
-    plotParamsC$typesInputC <- OneDimBrownianRandWalk(plotParamsC$stepsC,
-                                                      plotParamsC$meanC, plotParamsC$sdC)        
-  })
-  
+  plotParamsC <- reactiveValues(stepsC = 1, meanC = 0, sdC = 1, numbC=1, typesInputC = NA) 
+    observeEvent(input$runSimC, {
+              plotParamsC$meanC <- input$meanC
+              plotParamsC$sdC <- input$sdC
+              plotParamsC$stepsC <- input$stepsC
+              plotParamsC$numbC <- input$numbC
+              plotParamsC$typesInputC <- OneDimBrownianRandWalk(plotParamsC$stepsC,
+                                         plotParamsC$meanC, plotParamsC$sdC)        
+    })
   output$plotC <- renderPlot({    
     
     #### plot(xx$typesInputA ,type="l") 
     if (is.na(plotParamsC$typesInputC )) return(NULL)
-    plot(plotParamsC$typesInputC, type = "l")     
-  })
+    multiWalk(function(x) OneDimBrownianRandWalk(x,  plotParamsC$meanC, plotParamsC$sdC), plotParamsC$stepsC, plotParamsC$numbC)     
+    })
   
 ######2D SIMPLE RANDOM WALK  
-if (FALSE) {
-  # Return the requested dataset
-  typesInput <- reactive({
-    input$typesD
-  })
-}
 
-plotParamsD <- reactiveValues(stepsD = 1, typesInputD = NA) 
-observeEvent(input$runSimD, {
-  plotParamsD$stepsD <- input$stepsD
-  plotParamsD$xprobD <- input$xprobD
-  plotParamsD$yporbD <- input$yporbD
-  
-  plotParamsD$typesInputD <- TwoDimSimpleRandWalk(plotParamsD$stepsD, plotParamsD$xprobD, plotParamsD$yporbD)        
+  plotParamsD <- reactiveValues(stepsD = 1, xporbD = 0.5, yprobD = 0.5,typesInputD = NA) 
+    observeEvent(input$runSimD, {
+            plotParamsD$stepsD <- input$stepsD
+            plotParamsD$xprobD <- input$xprobD
+            plotParamsD$yporbD <- input$yporbD
+            plotParamsD$numbD <- input$numbD
+            plotParamsD$typesInputD <- TwoDimSimpleRandWalk(plotParamsD$stepsD, 
+                                                            plotParamsD$xprobD, plotParamsD$yporbD)        
 })
 
-output$plotD <- renderPlot({    
-  if (FALSE){
-    num.steps <- input$stepsD
-    x.prob <- input$xprobD
-    y.porb <- input$yprobD
-    typesInput <- TwoDimSimpleRandWalk(num.steps, x.prob, y.prob)        
-  }
+  output$plotD <- renderPlot({    
   
   #### plot(xx$typesInputA ,type="l") 
-  if (is.na(plotParamsD$typesInputD )) return(NULL)
-  plot(plotParamsD$typesInputD ,type="l")     
-})
-
+    if (is.na(plotParamsD$typesInputD )) return(NULL)
+    multiWalk(function(x) TwoDimSimpleRandWalk(x,  plotParamsD$xprobD, plotParamsD$yporbD), plotParamsD$stepsD, plotParamsD$numbD)     
+  })
 
 ######2D BROWNIAN RANDOM WALK  
-if (FALSE) {
-  # Return the requested dataset
-  typesInput <- reactive({
-    input$typesE
-  })
-}
 
-plotParamsE <- reactiveValues(stepsE = 1, typesInputE = NA) 
-observeEvent(input$runSimE, {
-  plotParamsE$stepsE <- input$stepsE
-  plotParamsE$XmeanE <- input$XmeanE
-  plotParamsE$XsdE <- input$XsdE
-  plotParamsE$YmeanE <- input$YmeanE
-  plotParamsE$YsdE <- input$YsdE
-  
-  
-  plotParamsE$typesInputE <- TwoDimBrownianRandWalk(plotParamsE$stepsE, plotParamsE$XmeanE, plotParamsE$XsdE, plotParamsE$YmeanE, plotParamsE$YsdE)        
+  plotParamsE <- reactiveValues(stepsE = 1, XmeanE = 0, XsdE =  1, YmeanE = 0, YsdE = 1, typesInputE = NA) 
+    observeEvent(input$runSimE, {
+           plotParamsE$stepsE <- input$stepsE
+           plotParamsE$XmeanE <- input$XmeanE
+           plotParamsE$XsdE <- input$XsdE
+           plotParamsE$YmeanE <- input$YmeanE
+           plotParamsE$YsdE <- input$YsdE
+           plotParamsE$numbE <- input$numbE
+           plotParamsE$typesInputE <- TwoDimBrownianRandWalk(plotParamsE$stepsE, 
+                                            plotParamsE$XmeanE, plotParamsE$XsdE, 
+                                            plotParamsE$YmeanE, plotParamsE$YsdE)        
 })
 
 output$plotE <- renderPlot({    
-  if (FALSE){
-    num.steps <- input$stepsE
-    x.mean <- input$XmeanE
-    x.sd <- input$XsdE
-    y.mean <- input$YmeanE
-    y.sd <- input$YsdE
-    typesInput <- TwoDimBrownianRandWalk(num.steps, x.mean, x.sd, y.mean, y.sd)        
-  }
-  
+
   #### plot(xx$typesInputA ,type="l") 
   if (is.na(plotParamsE$typesInputE )) return(NULL)
-  plot(plotParamsE$typesInputE ,type="l")     
+  multiWalk(function(x) TwoDimBrownianRandWalk(x,   plotParamsE$XmeanE, plotParamsE$XsdE, 
+                                               plotParamsE$YmeanE, plotParamsE$YsdE), 
+                                              plotParamsE$stepsE, plotParamsE$numbE)     
 })
+
+
+
+######2D Random Walk with Drift
+plotParamsG <- reactiveValues(xinitG = 1, yinitG = 1, deltaG = 0.0065,
+                                sigmaG = 0.005, typesInputG = NA)
+  observeEvent(input$runSimG, {
+               plotParamsG$xinitG <- input$xinitG
+               plotParamsG$yinitG <- input$yinitG
+               plotParamsG$numbG <- input$numbG
+               plotParamsG$stepsG <- 0
+               plotParamsG$deltaG <- 0.0065
+               plotParamsG$sigmaG <- 0.005
+               
+  plotParamsG$typesInputG <- TwoDimRandWalkDrift(plotParamsG$xinitG, plotParamsG$yinitG,
+                                                 plotParamsG$deltaG, plotParamsG$sigmaG)
+  })
+
+output$plotG <- renderPlot({
+  
+  if (is.na(plotParamsG$typesInputG)) return(NULL)
+  multiWalk(function(x) TwoDimRandWalkDrift(plotParamsG$xinitG, plotParamsG$yinitG, 
+                                            plotParamsG$deltaG, plotParamsG$sigmaG), 
+                                            plotParamsG$stepsG, plotParamsG$numbG)     
+})
+
 
 
 ######Great Circle RANDOM WALK  
 
-plotParamsF <- reactiveValues(lonBeginF = 0, lonEndF = 1, 
-                              latBeginF = 0, latEndF = 1, typesInputF = NA) 
-observeEvent(input$runSimF, {
-  plotParamsF$lonBeginF <- input$lonBeginF
-  plotParamsF$lonEndF <- input$lonEndF
-  plotParamsF$latBeginF <- input$latBeginF
-  plotParamsF$latEndF <- input$latEndF
-  
-  
-  plotParamsF$typesInputF <-GCRandomWalk( plotParamsF$lonBeginF, plotParamsF$latBeginF,
-                                          plotParamsF$lonEndF, plotParamsF$latEndF)        
+plotParamsF <- reactiveValues(stepsF = 0,latBeginF = 0, lonBeginF = 0, 
+                              latEndF = 1, lonEndF = 1,
+                              deltaG = 0.0065, sigmaG = 0.005,typesInputF = NA) 
+  observeEvent(input$runSimF, {
+              plotParamsF$latBeginF <- input$latBeginF
+               plotParamsF$lonBeginF <- input$lonBeginF
+               plotParamsF$latEndF <- input$latEndF
+               plotParamsF$lonEndF <- input$lonEndF
+               plotParamsF$numbF <- input$numbF
+               plotParamsF$stepsF <- 0
+               plotParamsF$deltaF <- 0.0065
+               plotParamsF$sigmaF <- 0.005               
+    
+  plotParamsF$typesInputF <-GCRandomWalk( plotParamsF$stepsF,
+                                          plotParamsF$latBeginF, plotParamsF$lonBeginF,
+                                          plotParamsF$latEndF, plotParamsF$lonEndF,
+                                          plotParamsF$deltaF, plotParamsF$sigmaF )        
 })
 
 output$plotF <- renderPlot({    
   
   #### plot(xx$typesInputA ,type="l") 
   if (is.na(plotParamsF$typesInputF )) return(NULL)
-  lonRange = abs(plotParamsF$lonBeginF - plotParamsF$lonEndF)
-  if (lonRange < 90) {
-    xlimF = c(plotParamsF$lonBeginF, plotParamsF$lonBeginF + 90 )
+           lonRange = abs(plotParamsF$lonBeginF - plotParamsF$lonEndF)
+  if (lonRange < 45) {
+           xlimF = c(plotParamsF$lonBeginF, plotParamsF$lonBeginF + 45 )
   } else xlimF = c(plotParamsF$lonBeginF,  plotParamsF$lonEndF)
   
-  plot(plotParamsF$typesInputF, xlim = xlimF, type = "l")     
+  multiWalk(function(x) GCRandomWalk(plotParamsF$stepsF,
+                                     plotParamsF$latBeginF, plotParamsF$lonBeginF,
+                                     plotParamsF$latEndF, plotParamsF$lonEndF,
+                                     plotParamsF$deltaF, plotParamsF$sigmaF), plotParamsF$stepsF, plotParamsF$numbF)     
 })
 
-
-  
 })
 
 
