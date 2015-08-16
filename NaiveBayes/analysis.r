d4e2e4716d7b072f732d0192773ba8c1410455a0
Duncan Temple Lang <duncan@r-project.org>
2015-08-16 08:19:07 -0700

    rewrite and simplification of the code

--- NaiveBayes/analysis.r
+++ NaiveBayes/analysis.r
@@ -1,5 +1,6 @@
 naiveBayes = function(handWritten){
-  
+
+      
   prob = list(1:784,1:784,1:784,1:784,1:784,1:784,1:784,1:784,1:784,1:784)
   
   for (i in 1:length(trainMean)){
@@ -25,6 +26,12 @@ naiveBayes = function(handWritten){
 }
 
 
+
+# From what I can make of this function,
+# it is the equivalent of
+#    vector[, ncol(vector):1]
+# but the column reordering is shorter, clearer, more efficient
+# and more general (not being fixed to 28 x 28 images)
 reverser = function(vector){
   new = rep(list(rep(0,28)), 28) 
   newVector = c()
@@ -39,11 +46,32 @@ reverser = function(vector){
   return(newVector)
 }
 
+
+# This is okay as is, but of course is using knowledge of the JSON content.
+# It is not clear that it has taken into account the first part of the path, i.e. the M(ove)
+# Instead,
+#  tmp = fromJSON(jsonFormat)
+#  tmp$objects[[1]]$path  # is the same as coordinates but with the directive (e.g., "Q", "M", "L")
+#  coordinatesList = lapply(tmp$objects[[1]]$path, function(x) unlist(x[-1]))
+# So
+#   tmp = fromJSON(jsonFormat)
+#   coordinatesList = lapply(tmp$objects[[1]]$path, function(x) unlist(x[-1]))
+#   coordinatesList = coordinatesList[ 2:(length(coordinatesList) - 1) ]
+# replaces the first 5 lines and also leaves us with more information about the steps and the overall canvas contents.
+#
+#   myMat = matrix(0, 28, 28) avoids rep(0, 784)   and why so many hard-coded numbers!
+#
+#  The loop is just extracting the i and i+1 elements from coordinatesList to get the row and column.
+#  So we can do this with
+#   coordinatesList = matrix(coordinatesList, , 2, byrow = TRUE)
+#   myMat[ coordinatesList ] = 1
+#   myMat[, ncol(myMat):1]
+
 jsonParser = function(jsonFormat){
   input = gsub("\"","",jsonFormat)
   coordinates = unlist(strsplit(input,"[]],[[][QL],"))
   coordinates = coordinates[2:length(coordinates)]
-  coordinates = coordinates[1:length(coordinates)-1]
+  coordinates = coordinates[1:length(coordinates)-1] #??? Why not do this in the previous line! Also, omitting the first and last coordinates from the array since not split from other text 
   coordinatesList = lapply(strsplit(coordinates,","),as.numeric)
   coordinatesList = lapply(coordinatesList, function(x) c( round(((x[1]+x[3])/2)*(28/300)),round(((x[2]+x[4])/2)*(28/300))))
   coordinatesFinal = unlist(coordinatesList)
@@ -54,3 +82,22 @@ jsonParser = function(jsonFormat){
   myMat = matrix(reverser(myMat),28,28)
   return(myMat)
 }
+
+
+library(RJSONIO)
+
+jsonParser1 =
+function(jsonFormat)
+{
+    tmp = fromJSON(jsonFormat)
+    coordinatesList = lapply(tmp$objects[[1]]$path, function(x) {
+                                                        x = unlist(x[-1])
+                                                        c( round(((x[1]+x[3])/2)*(28/300)),round(((x[2]+x[4])/2)*(28/300)))
+                                                    })
+    coordinatesList = matrix(unlist(coordinatesList), , 2, byrow = TRUE)
+    myMat = matrix(0, 28, 28)    
+    myMat[ coordinatesList ] = 1
+    myMat[, ncol(myMat):1]       # no need to define the reverser() function.
+}
+
+
