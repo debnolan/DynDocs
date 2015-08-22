2d82057230355628ca51812bee8b9777e2bc9fc5
Dennis Lee <yeoyeo@berkeley.edu>
2015-08-22 14:02:01 -0700

    I like this version. Code is cleaner. Hope it works well when augmented with the others.

--- SAT/Interactive Regression/server.R
+++ SAT/Interactive Regression/server.R
@@ -4,35 +4,29 @@ library(shiny)
 #setwd("~/DynDocs/SAT/Interactive Regression")
 load("data/satDF.rda")
 
-shinyServer(function(input, output) {
-  satDF$halves = cut(satDF$frac, breaks = 2)  
+shinyServer(function(input, output) {  
   
+  nums = list(1:25, 26:50, 1:15, 20:30, 35:50)
+
   lm_list = list()
-  for(i in 1:2){
-    lm_list[[i]] = lm(satDF$sat[which(satDF$halves==levels(satDF$halves)[i])]~ satDF$salary[which(satDF$halves==levels(satDF$halves)[i])], satDF)
+  for(i in 1:5){  
+  lm_list[[i]] = lm(satDF$sat ~ satDF$salary, subset = order(satDF$frac)[nums[[i]]])
   }
   
-  lm_list[[3]] = lm(satDF$sat ~ satDF$salary, subset = order(satDF$frac)[1:10])
-  lm_list[[4]] = lm(satDF$sat ~ satDF$salary, subset = order(satDF$frac)[25:35])
-  lm_list[[5]] = lm(satDF$sat ~ satDF$salary, subset = order(satDF$frac)[40:50])
-  
   party = list()
-  for(i in 1:2){
-  party[[i]] = satDF[which(satDF$halves==levels(satDF$halves)[i]) , ]
+  for(i in 1:5){
+  party[[i]] = satDF[order(satDF$frac)[nums[[i]]], ]
 }
 
-  party[[3]] = satDF[order(satDF$frac)[1:10], ]
-  party[[4]] = satDF[order(satDF$frac)[25:35], ]
-  party[[5]] = satDF[order(satDF$frac)[40:50], ]
-
-regression <- reactive({
+regression = reactive({
 
     if (input$model == "Simple regression") {
-      fit.res <- lm(satDF$sat~ satDF$salary, satDF)
+      fit.res = lm(satDF$sat~ satDF$salary, satDF)
     } else if (input$model == "Controlling for frac") {
       fit.res = list()
       fit.res[[1]] = lm_list[[3]]
       fit.res[[2]] = lm_list[[4]]
+      fit.res[[3]] = lm_list[[5]]
     } else if (input$model == "First Half of Data") {
       fit.res = lm_list[[1]] 
     } else if (input$model == "Second Half of Data") {
@@ -43,11 +37,11 @@ regression <- reactive({
     if (is.null(fit.res)) {
       fit.summary = NULL
    }  else {
-      
      if (class(fit.res) == "list") {
       fit.summary = list()
       fit.summary[[1]] = summary(fit.res[[1]])
       fit.summary[[2]] = summary(fit.res[[2]])
+      fit.summary[[3]] = summary(fit.res[[3]])    
      } else {
       fit.summary = summary(fit.res)  
      }
@@ -59,15 +53,14 @@ return(list(fit.res=fit.res, fit.summary=fit.summary))
   
   #---------------------------------------------------------------------------
   # Plot a scatter of the data with regression lines corresponding to the model
-  output$reg.plot <- renderPlot({         
+  output$reg.plot = renderPlot({         
   
     x = satDF$salary
     y = satDF$sat
     g = satDF$frac
     coefs = regression()$fit.res$coefficients
           
-  plot(x, y, pch=16, type= "n", cex=1.2, bty="n", main= "The Effect of Teacher Salary on SAT scores", xlab="Average Teacher Salary (in thousands of dollars)", ylab="Average Total SAT Scores")
-      
+  plot(x, y, pch=16, type= "n", cex=1.2, bty="n",xlim=c(25, 50.5), main= "The Effect of Teacher Salary on SAT scores", xlab="Average Teacher Salary (in thousands of dollars)", ylab="Average Total SAT Scores")
   
   if (input$model == "Simple regression") {
     points(x, y, pch=16, cex=1.2, col="black")
@@ -105,3 +98,5 @@ return(list(fit.res=fit.res, fit.summary=fit.summary))
     }
   })
 })
+
+##Credit: some ideas and code were adapted from mwaskom at https://github.com/mwaskom/ShinyApps/blob/master/multi_regression/server.R to illustrate this example. 
