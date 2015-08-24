3f223a57569e1616fec8fa0c0a6c1ce9b0d5d4fc
Dennis Lee <yeoyeo@berkeley.edu>
2015-08-24 11:00:33 -0700

    changed wording slightly

--- SAT/Interactive Regression/RunApp.R
+++ SAT/Interactive Regression/RunApp.R
@@ -2,22 +2,5 @@ library(shiny)
 
 runApp("Interactive Regression")
 
-setwd("~/DynDocs/sat")
-getwd()
-
-
-
-
-Two ideas to improve:
-  1) 7 drop down menu items. Subset the data by fraction and show each regression line for each subset. 
-      a) Highlight the subset from the original plot--each a different color. 
-  
-  2) Checkbox for each subset. Each subset has a different color. 
-      where you have the original data points. 
-          Then you can choose which sets of data you can include. 
-        Check each different box. Each box represents a different set. 
-          Combine Regressions. 
-
-  3) Optional: Slider for the data. Includes everything. 
-
-Zoes plot
+#setwd("~/DynDocs/sat")
+#getwd()
