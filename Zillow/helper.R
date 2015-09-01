918fd1db37c179d8284d6f8932a219e2cab6dc55
hazelmak <hazelmak@berkeley>
2015-08-31 21:35:10 -0700

    Zillow final version

--- Zillow/helper.R
+++ Zillow/helper.R
@@ -1,9 +1,12 @@
-
+#read in data and clean data
+library ("lattice")
+library ("mosaic")
+library ("ggplot2")
 real = read.delim("http://sites.williams.edu/rdeveaux/files/2014/09/Saratoga.txt")
 options(scipen=5)
 summary(real)
 
-real$Fuel.Type=factor(real$Fuel.Type,labels=c("Gas","Electric","Oil")) ###????why only 3 levels and number not maching the description
+real$Fuel.Type=factor(real$Fuel.Type,labels=c("Gas","Electric","Oil")) 
 real$Sewer.Type=factor(real$Sewer.Type,labels=c("None","Private","Public")) 
 real$Heat.Type=factor(real$Heat.Type,labels=c("Hot Air","Hot Water","Electric")) 
 real$Waterfront=factor(real$Waterfront,labels=c("No","Yes")) 
@@ -15,102 +18,186 @@ levels(real$Fireplaces)=c("None","1","2 or more","2 or more","2 or more")
 real$Beds=as.factor(real$Bedrooms)
 levels(real$Beds)=c("2 or fewer","2 or fewer","3","4 or more","4 or more","4 or more","4 or more")
 
+#choosePlot function
 
-choosePlot = function(varName = "Fireplace", plotType = "boxplot",
-  showing = "NA") {
+#choosePlot
+
+choosePlot = function(variable, plotType, showing = "All") {
   
-  if (plotType == "boxplot"){
-    real$Sewer.Type[real$Sewer.Type == "None"] = NA
-    if (varName == "Fireplace") {
+  if (identical(plotType, "boxplot")) {
+    
+    if (variable == "Fireplace") {
       xtitle = "Has Fireplace?"
     }
-    if (varName == "Central.Air") {
+    if (variable == "Central.Air") {
       xtitle = "Has Central Air?"
     }
-    if (varName == "Sewer.Type") {
-      xtitle = "Private or public?"
-    }
-    if (varName == "Beds") {
-      xtitle = "How Many Bedrooms?"
-    }
-    return (bwplot(real[["Price"]] ~ real[[varName]],
+    
+    return (bwplot(real[["Price"]] ~ real[[variable]],
                    xlab= xtitle, ylab="Price ($)"))
   }
   
-  if (plotType == "densityPlot") {
-    if (varName == "Fireplace") {
+  #density plot
+  if (identical(plotType, "densityPlot")) {
+    if (variable == "Fireplace") {
       if (showing =="All") {
-      plot (density (real$Price[real$Fireplace=="No"]),
-            xlab = "Price ($)", main = "Density of Price", col = "blue")
-      lines (density(real$Price[real$Fireplace=="Yes"]), col = "red")
-      legend (x = 400000, y = 0.000006, col = c("red", "blue"),
-              legend = c("With Fireplace", "Without Fireplace"),
-              pch = 15, pt.cex = 1, cex = 0.6)  
+        plot (density (real$Price[real$Fireplace=="No"]), xlab = "Price ($)", main = "Density of Price", col = "blue")
+        lines (density(real$Price[real$Fireplace=="Yes"]), col = "red")
+        legend (x = 500000, y = 0.000006, col = c("red", "blue"), legend = c("With Fireplace", "Without Fireplace"), pch = 15, pt.cex = 1, cex = 0.6)  
+      }
+      if (showing =="No") {
+        return (plot (density (real$Price[real$Fireplace=="No"]), xlab = "Price ($)", main = "Density of Price", col = "blue"))
+      }
+      if (showing =="Yes") {
+        return(plot (density (real$Price[real$Fireplace=="Yes"]), xlab = "Price ($)", main = "Density of Price", col = "Red"))
+      }    
     }
-    if (showing =="No") {
-      return (plot (density (real$Price[real$Fireplace=="No"]), xlab = "Price ($)", main = "Density of Price", col = "blue"))
+    #red for with fireplace, blue for without fireplace
+    
+    #central air
+    if (variable == "Central.Air") {
+      if (showing =="All") {
+        plot (density (real$Price[real$Central.Air=="No"]), xlab = "Price ($)", main = "Density of Price", col = "orange")
+        lines (density(real$Price[real$Central.Air=="Yes"]), col = "dark blue")
+        legend (x = 400000, y = 0.000006, col = c("dark blue", "orange"), legend = c("With Cantral Air", "Without Central Air"), pch = 15, pt.cex = 1, cex = 0.6)  
+      }
+      if (showing =="No") {
+        return(plot (density (real$Price[real$Central.Air=="No"]), xlab = "Price ($)", main = "Density of Price", col = "orange"))
+      }
+      if (showing =="Yes") {
+        return(plot (density (real$Price[real$Central.Air=="Yes"]), xlab = "Price ($)", main = "Density of Price", col = "dark blue"))
+      }    
     }
-    if (showing =="Yes") {
-      return(plot (density (real$Price[real$Fireplace=="Yes"]), xlab = "Price ($)", main = "Density of Price", col = "Red"))
-    }    
+    #dark blue for with central air, orange for without central air
   }
-  if (varName == "Central.Air") {
-    if (showing =="All") {
-      plot (density (real$Price[real$Central.Air=="No"]),
-            xlab = "Price ($)", main = "Density of Price", col = "orange")
-      lines (density(real$Price[real$Central.Air=="Yes"]), col = "dark blue")
-      legend (x = 400000, y = 0.000006, col = c("dark blue", "orange"),
-              legend = c("With Cantral Air", "Without Central Air"),
-              pch = 15, pt.cex = 1, cex = 0.6)  
+  
+  #scatterplot
+  if (identical(plotType, "scatterplot")) {
+    
+    if (variable == "Fireplace") {
+      if (showing =="All") {
+        return (xyplot(real$Price ~ real$Living.Area, pch=20, group=real[[variable]], 
+                       xlab="Living Area", ylab="Price ($)", 
+                       col = c("#f23320bb", "#000bf97a"), xlim = c(400, 5500), ylim = c(0, 800000),
+                       key=list(corner=c(1,1),
+                                lines=list(col=c("red","blue"), lty=c(2,2), lwd=6),
+                                text=list(c("With Fireplace","Without Fireplace")))))
+      }
+      if (showing =="No") {
+        return (xyplot(real$Price[real$Fireplace == showing] ~ real$Living.Area[real$Fireplace == showing], pch=20, 
+                       xlab="Living Area", ylab="Price ($)", 
+                       col = c("#000bf97a"), xlim = c(400, 5500), ylim = c(0, 800000)))
+      }
+      if (showing =="Yes") {
+        return (xyplot(real$Price[real$Fireplace == showing] ~ real$Living.Area[real$Fireplace == showing], pch=20, 
+                       xlab="Living Area", ylab="Price ($)", 
+                       col = c("#f23320bb"), xlim = c(400, 5500), ylim = c(0, 800000)))
+      }    
     }
-    if (showing =="No") {
-      return(plot (density (real$Price[real$Central.Air=="No"]),
-                   xlab = "Price ($)", main = "Density of Price", col = "orange"))
+    #red for with fireplace, blue for without fireplace
+    
+    #central air
+    if (variable == "Central.Air") {
+      if (showing =="All") {
+        return (xyplot(real$Price ~ real$Living.Area, pch=20, group=real[[variable]], 
+                       xlab="Living Area", ylab="Price ($)", 
+                       col = c("#ff7f009a", "#6e77ff7a"), xlim = c(400, 5500), ylim = c(0, 800000),  
+                       key=list(corner=c(1,1),
+                                lines=list(col=c("#6e77ff","#ff7f00"), lty=c(2,2), lwd=6),
+                                text=list(c("With Central Air","Without Central Air")))))
+      }
+      
+      if (showing =="No") {
+        return (xyplot(real$Price[real$Central.Air == showing] ~ real$Living.Area[real$Central.Air== showing], pch=20, 
+                       xlab="Living Area", ylab="Price ($)", 
+                       col = c("#ff7f009a"), xlim = c(400, 5500), ylim = c(0, 800000)))
+      }
+      if (showing =="Yes") {
+        return (xyplot(real$Price[real$Central.Air == showing] ~ real$Living.Area[real$Central.Air == showing], pch=20, 
+                       xlab="Living Area", ylab="Price ($)", 
+                       col = c("#6e77ff7a"), xlim = c(400, 5500), ylim = c(0, 800000)))
+      }    
     }
-    if (showing =="Yes") {
-      return(plot (density (real$Price[real$Central.Air=="Yes"]), xlab = "Price ($)", main = "Density of Price", col = "dark blue"))
-    }    
-  }
-  if (varName == "Sewer.Type") {
-    if (showing =="All") {
-      plot (density (real$Price[real$Sewer.Type=="Private"]),
-            xlab = "Price ($)", main = "Density of Price", col = "green")
-      lines (density(real$Price[real$Sewer.Type=="Public"]), col = "purple")
-      legend (x = 400000, y = 0.000006, col = c("purple", "green"),
-              legend = c("Public", "Private"), pch = 15, pt.cex = 1, cex = 0.6)  
-    }
-    if (showing =="Public") {
-      return(plot (density (real$Price[real$Sewer.Type=="Public"]),
-                   xlab = "Price ($)", main = "Density of Price", col = "purple"))
-    }
-    if (showing =="Private") {
-      return(plot (density (real$Price[real$Sewer.Type=="Private"]),
-                   xlab = "Price ($)", main = "Density of Price", col = "green"))
-    }    
-  }
-  if (varName == "Beds") {
-    if (showing =="All") {
-      plot (density (real$Price[real$Beds=="2 or fewer"]),
-            xlab = "Price ($)", main = "Density of Price", col = "#66c2a5")
-      lines (density(real$Price[real$Beds=="3"]), col = "#fc8d62")
-      lines (density(real$Price[real$Beds=="4 or more"]), col = "#8da0cb")
-      legend (x = 400000, y = 0.000006, col = c("#66c2a5", "#fc8d62", "#8da0cb"),
-              legend = c("2 or fewer", "3", "4 or more"), pch = 15,
-              pt.cex = 1, cex = 0.6)  
-    }
-    if (showing =="2 or fewer") {
-      return(plot (density (real$Price[real$Beds=="2 or fewer"]),
-                   xlab = "Price ($)", main = "Density of Price", col = "#66c2a5"))
-    }
-    if (showing =="3") {
-      return(plot (density (real$Price[real$Beds=="3"]),
-                   xlab = "Price ($)", main = "Density of Price",
-                   col = "#fc8d62"))
-    }
-    if (showing =="4 or more") {
-      return(plot (density (real$Price[real$Beds=="4 or more"]),
-                   xlab = "Price ($)", main = "Density of Price", col = "#8da0cb"))
-    }    
-  }
+    #dark blue for with central air, orange for without central air
   }
 }
+
+
+#linear regression
+RegreF = function(varName,showing1){
+  if (identical (varName, "Fireplace")) {
+  if (showing1 =="All") {
+  return (xyplot(real$Price ~ real$Living.Area, pch=20, group=real[[varName]], 
+  xlab="Living Area", ylab="Price ($)", col = c("#f23320bb", "#000bf97a"), xlim = c(400, 5500), ylim = c(0, 800000),
+  panel = function(x, y, ...) {
+  panel.superpose(x, y, ..., panel.groups = function(x,y, col, col.symbol, ...) {
+                                         panel.xyplot(x, y, col=col.symbol, ...)
+                                         panel.abline(lm(y~x), col.line=col.symbol)
+                                              }
+                       )
+                     },
+                     key=list(corner=c(1,1),
+                              lines=list(col=c("red","blue"), lty=c(2,2), lwd=6),
+                              text=list(c("With Fireplace","Without Fireplace")))))
+    }
+    if (showing1 =="No") {
+      return (xyplot(real$Price[real$Fireplace == showing1] ~ real$Living.Area[real$Fireplace == showing1], pch=20, 
+                     xlab="Living Area", ylab="Price ($)", 
+                     panel = function(x, y, ...) {
+                       panel.xyplot(x, y, ...)
+                       panel.abline(lm(y~x))},
+                     col = c("#000bf97a"), xlim = c(400, 5500), ylim = c(0, 800000)))
+    }
+    if (showing1 =="Yes") {
+      return (xyplot(real$Price[real$Fireplace == showing1] ~ real$Living.Area[real$Fireplace == showing1], pch=20, 
+                     xlab="Living Area", ylab="Price ($)", 
+                     panel = function(x, y, ...) {
+                       panel.xyplot(x, y, ...)
+                       panel.abline(lm(y~x))},
+                     col = c("#f23320bb"), xlim = c(400, 5500), ylim = c(0, 800000)))
+    }    
+  }
+  
+  if (identical (varName, "Central.Air")) {
+    if (showing1 =="All") {
+      return (xyplot(real$Price ~ real$Living.Area, pch=20, group=real[[varName]], 
+                     xlab="Living Area", ylab="Price ($)", 
+                     col = c("#ff7f009a", "#6e77ff7a"), xlim = c(400, 5500), ylim = c(0, 800000),
+                     panel = function(x, y, ...) {
+                       panel.superpose(x, y, ...,
+                                       panel.groups = function(x,y, col, col.symbol, ...) {
+                                         panel.xyplot(x, y, col=col.symbol, ...)
+                                         panel.abline(lm(y~x), col.line=col.symbol)
+                                       }
+                       )
+                     },
+                     key=list(corner=c(1,1),
+                              lines=list(col=c("#6e77ff","#ff7f00"), lty=c(2,2), lwd=6),
+                              text=list(c("With Central Air","Without Central Air")))))
+    }
+    
+    if (showing1 =="No") {
+      return (xyplot(real$Price[real$Central.Air == showing1] ~ real$Living.Area[real$Central.Air== showing1], pch=20, 
+                     xlab="Living Area", ylab="Price ($)",
+                     panel = function(x, y, ...) {
+                       panel.xyplot(x, y, ...)
+                       panel.abline(lm(y~x))},
+                     col = c("#ff7f009a"), xlim = c(400, 5500), ylim = c(0, 800000)))
+    }
+    if (showing1 =="Yes") {
+      return (xyplot(real$Price[real$Central.Air == showing1] ~ real$Living.Area[real$Central.Air == showing1], pch=20, 
+                     xlab="Living Area", ylab="Price ($)",
+                     panel = function(x, y, ...) {
+                       panel.xyplot(x, y, ...)
+                       panel.abline(lm(y~x))},
+                     col = c("#6e77ff7a"), xlim = c(400, 5500), ylim = c(0, 800000)))
+    }    
+  }
+}
+
+
+
+
+
+
+
--- Zillow/server.R
+++ Zillow/server.R
@@ -1,20 +1,31 @@
 # server.R
-
+library (shiny)
 library(lattice)
 library(mosaic)
 library(ggplot2)
 source("helper.R")
 
+
 shinyServer(function(input, output) {
-
-  plotParams <- reactiveValues(plotT = NULL, varN = NULL)
-
-  observeEvent( input$makePlot, {
-      plotParams$plotT = input$plotType
-      plotParams$varN = input$varName
-    })
+  
+  plotParams <- reactiveValues(plotT = NULL, varN = NULL, show = NULL, var = NULL, show1 = NULL)
+  
+  observeEvent(input$makePlot, {
+    plotParams$plotT = input$plotType
+    plotParams$varN = input$variable
+    plotParams$show = input$showing
+  })
   
   output$plotA <- renderPlot({  
-    choosePlot(plotParams$varN, plotParams$plotT , showing = "All")
+    choosePlot(plotParams$varN, plotParams$plotT , plotParams$show)
+  })
+ 
+  observeEvent(input$makePlot2, {
+    plotParams$var = input$varName
+    plotParams$show1 = input$showing1
+  })
+  
+  output$plotB <- renderPlot({  
+    RegreF(plotParams$var, plotParams$show1)
   })
 })
